ba566e303732758679773847d7000e4d0c5f1b31
Tian Zhang <tianzhang@dhcp-10-105-182-143.wireless.northwestern.private>
2015-10-13 21:16:26 -0500

    initial

---
+++ QueueByStack.py
@@ -0,0 +1,46 @@
+__author__ = 'tianzhang'
+
+# Implement a queue with two stacks
+
+
+class queue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, x):
+        if len(self.stack1) == 0:
+            while len(self.stack2) > 0:
+                self.stack1.append(self.stack2.pop())
+        self.stack1.append(x)
+
+    def dequeue(self):
+        if len(self.stack2) == 0:
+            while len(self.stack1) > 1:
+                self.stack2.append(self.stack1.pop())
+            return self.stack1.pop()
+        else:
+            return self.stack2.pop()
+
+    def output(self):
+        if len(self.stack1) > 0:
+            print self.stack1
+        else:
+            print self.stack2[::-1]
+
+
+if __name__ == '__main__':
+    events = [('e', 0), ('d', -1), ('e', 1), ('e', 2), ('e', 3), ('d', -1), ('e', 4), ('d', -1), ('d', -1), ('d', -1), ('d', -1), ('e', 5)]
+    test = queue()
+    for event in events:
+        try:
+            if event[0] == 'e':
+                test.enqueue(event[1])
+                print 'Enqueue ' + str(event[1])
+                test.output()
+            else:
+                print 'Dequeue ' + str(test.dequeue())
+                test.output()
+        except IndexError as e:
+            print e
+            continue
---
+++ StackByQueue.py
@@ -0,0 +1,11 @@
+__author__ = 'tianzhang'
+
+# Implement a stack with two queues
+import Queue
+
+
+class stack:
+    def __init__(self):
+        queue1 = Queue.Queue()
+        queue2 = Queue.Queue()
+
